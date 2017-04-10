08d1a623d0f645b05958e636b5359abfea2e60af
DangerBlack <danger.recheng@hotmail.it>
2017-04-10 18:43:48 +0200

    bugfix

--- php/checkUpdate.php
+++ php/checkUpdate.php
@@ -9,10 +9,8 @@
     if(getRole()<=1){
         @$remoteV = file_get_contents('https://raw.githubusercontent.com/DangerBlack/homeflix/master/installer/version.txt');
         $remoteV = floatval($remoteV);
-        echo "remoteV".$remoteV."-";
         $v = getVersion();
         $v = floatval($v);
-        echo "localV".$v."-";
         if($remoteV>$v){
             echo $remoteV;
         }else{
